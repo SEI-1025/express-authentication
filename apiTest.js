d8d2110ecfb1ea00d0006c9bf66e468260a6f0ec
Rome Bell <romebell5@gmail.com>
2021-12-08 18:46:12 -0800

    add apiTest.js

---
+++ apiTest.js
@@ -0,0 +1,6 @@
+require('dotenv').config();
+const axios = require('axios');
+
+function getArtistOnSpotify(endpoint) {}
+function getAlbumOnSpotify(endpoint) {}
+function getSongOnSpotify(endpoint) {}
